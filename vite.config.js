1e0d85fcdf33ed47dba7ce91d4da2ff7f0cedcc3
yiyang <lyy845283876@live.com>
2026-06-11 17:47:55 +0800

    使用vite构建项目

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react({
+    jsxRuntime: 'classic',
+  })],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+  define: {
+    'process.env': process.env,
+  },
+});
